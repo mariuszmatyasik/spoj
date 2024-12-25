58ab4c05914eceb165f3f4a4249756b15d3e00c5
mariuszmatyasik <mariusz.matyasik@microsoft.wsei.edu.pl>
2024-12-25 23:08:01 +0100

    spoj.FCTRL2 recursive solution

---
+++ spoj.FCTRL2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+public class Program
+{
+    static BigInteger Factorial(BigInteger n)
+    {
+        if (n <= 1) return 1;
+        return n * Factorial(n - 1);
+    }
+    public static void Main()
+    {
+        int t = Int32.Parse(Console.ReadLine());
+
+        for (var i = 0; i < t; i++)
+        {
+            var n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(n));
+        }
+    }
+}
