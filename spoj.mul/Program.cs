a0011ff5858792af08935145d003c62741e2e698
mariuszmatyasik <mariusz.matyasik@microsoft.wsei.edu.pl>
2024-12-25 22:38:52 +0100

    spoj.mul loop

---
+++ spoj.mul/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] s = Console.ReadLine().Split();
+
+            Console.WriteLine(BigInteger.Parse(s[0]) * BigInteger.Parse(s[1]));
+        }
+    }
+}
